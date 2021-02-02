c31aa95a796fb43294c91daaaef815743cd698f0
1421320187@qq.com <1421320187@qq.com>
2021-02-02 16:28:13 +0800

    requests封装

--- Tool/http_request.py
+++ Tool/http_request.py
@@ -6,4 +6,24 @@ import requests
 class Http_request:
 
     def __init__(self):
-        pass
+        self.headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"}
+        self.res = requests.session()  # 实例化一个会话实例
+
+    def __del__(self):
+        """关闭会话"""
+        self.res.close()
+
+    def send_request(self,url,params=None,data=None,headers=None,methond="get",cookie=None,verify=False):
+
+        if methond.lower()=="get":
+            response = self.res.get(url,params=params,headers=self.headers)
+        else:
+            response = self.res.post(url,data=data,headers=headers,cookies=cookie,verify=verify)
+        return response
+
+
+if __name__ == "__main__":
+    res = Http_request()
+    loginpage_url = "https://demo.fastadmin.net/index/user/login.html"
+    response = res.send_request(loginpage_url,methond="get")
+    print(response)
