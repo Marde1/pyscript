7d48d7303c911ecc3900b7d9ebb74e12eb4411c8
1421320187@qq.com <1421320187@qq.com>
2021-03-09 18:15:34 +0800

    webTours

---
+++ WebTours/TestCaseDDT.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+from ddt import ddt,data,unpack
+import unittest
+
+@ddt #装饰测试类
+class TestCaseDDT(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @data(1) #方法装饰器
+    def test_api(self,i):
+        print(i)
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
