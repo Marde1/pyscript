27cecf3dd822c5a4bdeca741d1a8e46ea1ecc0ad
1421320187@qq.com <1421320187@qq.com>
2021-03-15 18:12:34 +0800

    webTours

--- WebTours/TestCaseDDT.py
+++ WebTours/TestCaseDDT.py
@@ -2,20 +2,53 @@
 
 from ddt import ddt,data,unpack
 import unittest
+from Tool.excelRead import ExcelRead
+import re
+from Tool.http_request import Http_request #导入Http_request类
+
+userSession = None
+
+testdata = ExcelRead("webTour_data.xlsx","data").readData()
 
 @ddt #装饰测试类
 class TestCaseDDT(unittest.TestCase):
 
-    def __init__(self,testdata):
-        super(self,TestCaseDDT).__init__()
-        self.testdata = testdata
+    welcomePage_url = "http://localhost:1080/cgi-bin/welcome.pl?signOff=true"
+    homePage_url = "http://localhost:1080/cgi-bin/nav.pl?in=home"  # 获取响应中form表单中的userSession值
+    req = Http_request()
 
     def setUp(self):
-        pass
+        global userSession
+        welcomePage_response = self.req.send_request(self.welcomePage_url, method="get")
+        welcome_cookies = welcomePage_response.cookies
+        homePage_response = self.req.send_request(self.homePage_url, method="get", cookies=welcome_cookies)
+        userSession = re.findall('<input type="hidden" name="userSession" value="(.*?)"/>', homePage_response.text)
+        # data_dict = eval(self.data)
+        self.data["userSession"] = userSession[0]  # 从响应中获取的值再塞入到data数据中
 
-    @data(1) #方法装饰器
-    def test_api(self,i):
-        print(i)
+    @data(*testdata) #方法装饰器
+    # @unpack #脱外套
+    def test_api(self,data):
+        dict_data = eval(data)
+        print()
+        print("测试用例编号:{}".format(dict_data["testcase_id"]))
+        print("测试功能模块:{}".format(dict_data["function_module"]))
+        print("测试用例标题:{}".format(dict_data["testcase_title"]))
+        print("测试数据url:{}".format(dict_data["url"]))
+        print("测试数据data:{}".format(dict_data["function_module"]))
+        print("测试数据method:{}".format(dict_data["method"]))
+        print("预期结果:{}".format(dict_data["expected"]))
+        login_response = self.req.send_request(self.url, method=self.method, data=self.data)
+        # print(login_response.text)
+        login_list = re.findall('<frame src="(.*?)" name="info"', login_response.text)
+        login_str = login_list[0]
+        # print(login_list[0])
+        print("实际结果:", login_str)
+        try:
+            self.assertEqual(login_str, self.expected)
+        except AssertionError as e:
+            print("出现异常：", e)
+            raise e
 
     def tearDown(self):
         pass
--- WebTours/toursTestDDTRunner.py
+++ WebTours/toursTestDDTRunner.py
@@ -8,4 +8,11 @@ if __name__ == "__main__":
     suite = unittest.TestSuite()
     loader = unittest.TestLoader()
     suite.addTest(loader.loadTestsFromTestCase(TestCaseDDT))
+    with open("WebToursResult.html",mode="wb") as file:
+        runner = HTMLTestRunnerNew.HTMLTestRunner(stream=file,
+                                                verbosity=2,
+                                                title="WebTours登录接口测试DDT",
+                                                description=None,
+                                                tester=None)
+        runner.run(suite)
 
