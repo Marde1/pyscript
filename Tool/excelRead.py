eec7ee0f2d7e905d20372c3a8bdcc2ec35ff92b9
1421320187@qq.com <1421320187@qq.com>
2021-03-22 18:58:41 +0800

    webTours

--- Tool/excelRead.py
+++ Tool/excelRead.py
@@ -5,19 +5,21 @@ import openpyxl
 class ExcelRead():
 
     def __init__(self,filename,sheetname):
-        self.wb = openpyxl.load_workbook(filename)
+        self.filename = filename
+        self.wb = openpyxl.load_workbook(self.filename)
         self.sheet = self.wb[sheetname]
+        self.maxrow_num = self.sheet.max_row
+        self.maxcolumn_num = self.sheet.max_column
 
     def readData(self):
-        maxrow_num = self.sheet.max_row
-        maxcolumn_num = self.sheet.max_column
-        # print("最大行：",maxrow_num)
-        # print("最大列：",maxcolumn_num)
+
+        # print("最大行：",self.maxrow_num)
+        # print("最大列：",self.maxcolumn_num)
         data_dict = {}
         data_list =[]
-        if(maxrow_num>0 and maxcolumn_num>0):
-            for i in range(2,maxrow_num+1): #行
-                for j in range(1,maxcolumn_num+1):#列
+        if(self.maxrow_num>0 and self.maxcolumn_num>0):
+            for i in range(2,self.maxrow_num+1): #行
+                for j in range(1,self.maxcolumn_num+1):#列
                     # print(i,j)
                     # print(self.sheet.cell(1,j).value)
                     # print(self.sheet.cell(i,j).value)
@@ -27,7 +29,27 @@ class ExcelRead():
                 data_dict = {} #数据存入列表后清空字典，以便后面的数据存入
         return data_list
 
-# if __name__ == "__main__":
-#     excel = ExcelRead(r"..\WebTours\webTour_data.xlsx","data")
-#     list01 = excel.readData()
-#     print(list01)
+    def updateData(self,rownum,columnnum,value_str):
+        """
+        向excel中修改数据
+        :param rownum: 行
+        :param columnnum:列
+        :param value_str: 需要写入的字符串
+        :return:
+        """
+        self.sheet.cell(rownum,columnnum).value = value_str
+        self.wb.save(self.filename)
+
+    def updateResultData(self,rownum,value_str):
+        """
+        向excel中修改excle的最后一列的数据数据
+        :param rownum: 行
+        :param value_str: 需要写入的字符串
+        :return:
+        """
+        self.updateData(rownum,self.maxcolumn_num,value_str)
+
+if __name__ == "__main__":
+    excel = ExcelRead(r"..\WebTours\webTour_data.xlsx","data")
+    list01 = excel.readData()
+    print(list01)
--- WebTours/TestCaseDDT.py
+++ WebTours/TestCaseDDT.py
@@ -8,7 +8,8 @@ from Tool.http_request import Http_request #导入Http_request类
 
 userSession = None
 
-testdata = ExcelRead("webTour_data.xlsx","data").readData()
+data_sheet = ExcelRead("webTour_data.xlsx","data");
+testdata = data_sheet.readData()
 
 @ddt #装饰测试类
 class TestCaseDDT(unittest.TestCase):
@@ -23,14 +24,16 @@ class TestCaseDDT(unittest.TestCase):
         welcome_cookies = welcomePage_response.cookies
         homePage_response = self.req.send_request(self.homePage_url, method="get", cookies=welcome_cookies)
         userSession = re.findall('<input type="hidden" name="userSession" value="(.*?)"/>', homePage_response.text)
-        # data_dict = eval(self.data)
-        self.data["userSession"] = userSession[0]  # 从响应中获取的值再塞入到data数据中
+        # self.data = eval(data)
+        # self.data["userSession"] = userSession[0]  # 从响应中获取的值再塞入到data数据中
 
     @data(*testdata) #方法装饰器
     # @unpack #脱外套
-    def test_api(self,data):
-        dict_data = eval(data)
+    def test_api(self,dict_data):
+        # dict_data = eval(data)
         print()
+        data = eval(dict_data["data"])
+        data["userSession"]= userSession[0]
         print("测试用例编号:{}".format(dict_data["testcase_id"]))
         print("测试功能模块:{}".format(dict_data["function_module"]))
         print("测试用例标题:{}".format(dict_data["testcase_title"]))
@@ -38,14 +41,16 @@ class TestCaseDDT(unittest.TestCase):
         print("测试数据data:{}".format(dict_data["function_module"]))
         print("测试数据method:{}".format(dict_data["method"]))
         print("预期结果:{}".format(dict_data["expected"]))
-        login_response = self.req.send_request(self.url, method=self.method, data=self.data)
+        login_response = self.req.send_request(dict_data["url"], method=dict_data["method"], data=data)
         # print(login_response.text)
         login_list = re.findall('<frame src="(.*?)" name="info"', login_response.text)
         login_str = login_list[0]
         # print(login_list[0])
         print("实际结果:", login_str)
         try:
-            self.assertEqual(login_str, self.expected)
+            self.assertEqual(login_str, dict_data["expected"],msg="fail")
+            data_sheet.updateResultData(dict_data["testcase_id"]+1,"pass")
+
         except AssertionError as e:
             print("出现异常：", e)
             raise e
